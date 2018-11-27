3a5e4c62f3aac438f68124cfa926c7b276499414
alik <anothermalik@gmail.com>
2018-11-28 05:27:29 +0700

    checkpoint 1.1

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,6 @@
 import Knex from './knex';
 import jwt from 'jsonwebtoken';
 
-// The idea here is simple: export an array which can be then iterated over and each route can be attached. 
 const routes = [
     {
         method: 'GET',
@@ -12,9 +11,8 @@ const routes = [
     },
     {
         method: 'GET',
-        path: '/test',
+        path: '/users',
         handler: (request, reply) => {
-            // In general, the Knex operation is like Knex('TABLE_NAME').where(...).chainable(...).then(...)
             const getOperation = Knex('users').select().then((results) => {
                 if (!results || results.length === 0) {
                     return {
@@ -35,8 +33,10 @@ const routes = [
     {
         path: '/auth',
         method: 'POST',
+        config: {
+            auth: false
+        },
         handler: (request, reply) => {
-            // This is a ES6 standard
             const {
                 email,
                 password
@@ -50,10 +50,8 @@ const routes = [
                         errMessage: 'the specified user was not found',
                     };
                 }
-                // Honestly, this is VERY insecure. Use some salted-hashing algorithm and then compare it.
                 if (user.password === password) {
                     const token = jwt.sign({
-                        // You can have anything you want here. ANYTHING. As we'll see in a bit, this decoded token is passed onto a request handler.
                         email
                     }, 'vZiYpmTzqXMp8PpYXKwqc9ShQ1UhyAfy', {
                         algorithm: 'HS256',
@@ -71,19 +69,222 @@ const routes = [
             return getOperation;
         }
     },
-    // {
-    //     path: '/speed',
-    //     method: 'POST',
-    //     config: {
-    //         auth: {
-    //             strategy: 'jwt',
-    //         }
-    //     },
-    //     handler: (request, reply) => {
-    //         const {
-    //             id_user
-    //         } = request.payload;
-    //     }
-    // }
+    {
+        path: '/createuser',
+        method: 'POST',
+        handler: (request, reply) => {
+            const {name,email,password} = request.payload;
+            const token = jwt.sign({
+                email
+            }, 'vZiYpmTzqXMp8PpYXKwqc9ShQ1UhyAfy', {
+                algorithm: 'HS256',
+                expiresIn: '1h',
+            });
+            const row = {
+                name,
+                email,
+                password,
+                role: 1,
+                created_at: new Date(),
+                token
+            };
+            const insertOperation = Knex('users').insert(row).then((results) => {
+                return{
+                    message: 'successfully created user'
+                };
+            }).catch((err) => {
+                console.log(err);
+                return err;
+            });
+            return insertOperation;
+        }
+    },
+    {
+        path: '/createadmin',
+        method: 'POST',
+        handler: (request, reply) => {
+            const {
+                name,
+                email,
+                password,
+            } = request.payload;
+            const token = jwt.sign({
+                email
+            }, 'vZiYpmTzqXMp8PpYXKwqc9ShQ1UhyAfy', {
+                algorithm: 'HS256',
+                expiresIn: '1h',
+            });
+            const row = {
+                name,
+                email,
+                password,
+                role: 0,
+                created_at: new Date(),
+                token
+            };
+            const insertOperation = Knex('users').insert(row).then((results) => {
+                return {
+                    message: 'successfully created admin'
+                };
+            }).catch((err) => {
+                console.log(err);
+                return err;
+            });
+            return insertOperation;
+        }
+    },
+    {
+        method: 'GET',
+        path: '/user/{id}',
+        handler: (request, h) => {
+            const getOperation = Knex('users').where({id:request.params.id}).select().then((results) => {
+                return {
+                    data: results,
+                };
+            }).catch((err) => {
+                return err;
+            });
+            return getOperation;
+        }
+    },
+    {
+        method: 'GET',
+        path: '/deleteuser/{id}',
+        handler: (request, h) => {
+            const getOperation = Knex('users').where({
+                id: request.params.id
+            }).del().then((results) => {
+                return {
+                    results,
+                };
+            }).catch((err) => {
+                return err;
+            });
+            return getOperation;
+        }
+    },
+    {
+        method: 'POST',
+        path: '/updateuser/{id}',
+        handler: (request, h) => {
+            const {name,email,password} = request.payload;
+            const row = {
+                name,
+                email,
+                password
+            };
+            const getOperation = Knex('users').where({
+                id: request.params.id
+            }).update(row).then((results) => {
+                return {
+                    results
+                };
+            }).catch((err) => {
+                return err;
+            });
+            return getOperation;
+        }
+    },
+    {
+        method: 'POST',
+        path: '/insertspeed',
+        handler: (request, h) => {
+            const {id_user,speed} = request.payload;
+            const row = {
+                id_user, 
+                speed
+            };
+            const getOperation = Knex('speed').where({
+                id: request.params.id
+            }).insert(row).then((results) => {
+                return {
+                    message: 'successfully insert speed'
+                };
+            }).catch((err) => {
+                return err;
+            });
+            return getOperation;
+        } 
+    },
+    {
+        method: 'GET',
+        path: '/getspeed',
+        handler: (request, h) => {
+            const getOperation = Knex('speed').select().then((results) => {
+                return {
+                    results
+                };
+            }).catch((err) => {
+                return err;
+            });
+            return getOperation;
+        }
+    },
+    {
+        method: 'POST',
+        path: '/updatespeed/{id}',
+        handler: (request, h) => {
+            const {id_user,speed} = request.payload;
+            const row = {id_user,speed};
+            const getOperation = Knex('speed').where({
+                id: request.params.id
+            }).update(row).then((results) => {
+                return {
+                    results
+                };
+            }).catch((err) => {
+                return err;
+            });
+            return getOperation;
+        }
+    },
+    {
+        method: 'GET',
+        path: '/deletespeed/{id}',
+        handler: (request, h) => {
+            const getOperation = Knex('speed').where({
+                id: request.params.id
+            }).del().then((results) => {
+                return {
+                    results,
+                };
+            }).catch((err) => {
+                return err;
+            });
+            return getOperation;
+        }
+    },
+    {
+        method: 'POST',
+        path: '/login',
+        handler: (request, h) => {
+            const getOperation = Knex('users').where({
+                id: request.params.id
+            }).del().then((results) => {
+                return {
+                    results,
+                };
+            }).catch((err) => {
+                return err;
+            });
+            return getOperation;
+        }
+    },
+    {
+        method: 'GET',
+        path: '/logout',
+        handler: (request, h) => {
+            const getOperation = Knex('users').where({
+                id: request.params.id
+            }).del().then((results) => {
+                return {
+                    results,
+                };
+            }).catch((err) => {
+                return err;
+            });
+            return getOperation;
+        }
+    }
 ]
 export default routes;
--- src/server.js
+++ src/server.js
@@ -1,22 +1,11 @@
 import Hapi from 'hapi';
 import routes from './routes';
 
-const people = { // our "users database"
-    1: {
-        id: 1,
-        name: "Abdul Malik",
-        role: 0,
-        email: "alik@gmail.com",
-        password: "password",
-        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFsaWtAZ21haWwuY29tIiwiaWF0IjoxNTQzMzQ4MjYwLCJleHAiOjE1NDMzNTE4NjB9.6LE4B3PBd3rPvBXKQUPysjKF8u4pqi5Ec9bUSfYBWHI",
-        created_at: "2018-11-27T17:37:16.000Z"
-    }
-};
-
 // bring your own validation function
 const validate = async (decoded, request) =>{
     // do your checks to see if the person is valid
-    if (!people[decoded.id]) {
+    // console.log(decoded);
+    if (!decoded.email) {
         return {
             isValid: false
         };
@@ -32,15 +21,15 @@ const init = async() => {
         port: 8000
     });
 
-    // await server.register(require('hapi-auth-jwt2'));
-    // server.auth.strategy('token', 'jwt', {
-    //     key: 'vZiYpmTzqXMp8PpYXKwqc9ShQ1UhyAfy',
-    //     validate,
-    //     verifyOptions: {
-    //         algorithms: ['HS256'],
-    //     }
-    // });
-    // server.auth.default('token');
+    await server.register(require('hapi-auth-jwt2'));
+    server.auth.strategy('token', 'jwt', {
+        key: 'vZiYpmTzqXMp8PpYXKwqc9ShQ1UhyAfy',
+        validate,
+        verifyOptions: {
+            algorithms: ['HS256'],
+        }
+    });
+    server.auth.default('token');
 
     routes.forEach((route) => {
         console.log(`attaching ${route.path}`);
@@ -55,18 +44,3 @@ init().then((server) => console.log(`Server listening on ${server.info.uri}`))
     console.error(err);
     process.exit(1);
 });
-
-
-
-// Start the server
-// async function start() {
-//     try {
-//         await server.start();
-//     } catch (err) {
-//         console.log(err);
-//         process.exit(1);
-//     }
-//     console.log('Server running at:', server.info.uri);
-// };
-
-// start();
